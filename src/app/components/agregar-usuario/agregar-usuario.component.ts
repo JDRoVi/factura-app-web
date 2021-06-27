25b66504cecf280992b92aae33b0b9ce3d7747e7
Michael ys <meys98@outlook.com>
2021-06-27 14:58:14 -0600

    update
    employe add, proveedor add, user add,

--- src/app/components/agregar-usuario/agregar-usuario.component.ts
+++ src/app/components/agregar-usuario/agregar-usuario.component.ts
@@ -2,20 +2,38 @@ import { Component, OnInit } from '@angular/core';
 import { timer } from 'rxjs';
 import { Usuario } from '../../models/usuario';
 import { UsuarioService } from '../../services/usuario.service';
+import { EmpleadoService } from 'src/app/services/empleado.service';
 
 @Component({
   selector: 'app-agregar-usuario',
   templateUrl: './agregar-usuario.component.html',
   styleUrls: ['./agregar-usuario.component.css'],
-  providers: [UsuarioService]
+  providers: [UsuarioService,EmpleadoService]
 })
 export class AgregarUsuarioComponent implements OnInit {
   public user: Usuario;
   public status: number;
+  public employes:any;
 
-  constructor(private _usuarioService: UsuarioService) {
+  constructor(private _usuarioService: UsuarioService,private _employeService:EmpleadoService) {
     this.user = new Usuario(1, 1, "", "", 1);
     this.status = -1;
+    this.loadEmployes();
+  }
+
+  public loadEmployes() {
+    this._employeService.getEmployees().subscribe(
+      response => {
+        console.log(response);
+        if (response.code == 200) {
+          this.employes = response.data;
+        }
+      },
+        error => {
+          this.employes = null;
+          console.log(error);
+        }
+    );
   }
 
   ngOnInit(): void {
