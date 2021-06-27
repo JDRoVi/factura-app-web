3dc218a2e8d2e7e92ff36c30cd1ac940e035b073
Jose Daniel Rodriguez Villalobos <68168528+JDRoVi@users.noreply.github.com>
2021-06-27 10:53:07 -0600

    Adelanto de funciones de agregar y mostrar

--- src/app/models/usuario.ts
+++ src/app/models/usuario.ts
@@ -1,6 +1,7 @@
 export class Usuario {
     constructor(
         public id: number,
+        public idEmpleado: number,
         public nombreUsuario: string,
         public password: string,
         public nivelUsuario: number
